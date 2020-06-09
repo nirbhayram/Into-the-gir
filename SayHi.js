e0f52ad081b4bf988e9c874a337d31d6da4771f9
Nirbhay <da201501068@gmail.com>
2020-06-09 20:04:44 +0530

    redirect to diffrent pages.

--- SayHi.js
+++ SayHi.js
@@ -1,3 +1,18 @@
 function sayHi(){
   alert("Hi!");
 }
+function over() {
+   document.write ("Mouse Over");
+}
+function out() {
+   document.write ("Mouse Out");
+}
+function reDirectToGoogle(){
+  window.location="https://google.com";
+}
+function getConfirmationForRedirect(){
+  var redirectValue = confirm("Do you want to redirect to google?");
+  if (redirectValue == true) {
+    reDirectToGoogle();
+  }
+}
