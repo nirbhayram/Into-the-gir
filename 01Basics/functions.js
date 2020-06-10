0935e4abf50f04e7ec5d7b75066e55b2c8322e6a
Nirbhay <da201501068@gmail.com>
2020-06-10 22:51:45 +0530

    changes in function js file.

--- 01Basics/functions.js
+++ 01Basics/functions.js
@@ -1,16 +1,17 @@
 let months = ['jan','feb','mar','apr','may','jun','jul','aug','sept','oct','nov','dec'];
 
-
+//Function with parameter
 let firstFunction = function (param){
     console.log(`first function with this parameter ${param}`);
 };
 months.forEach(firstFunction);
 
-
+//literals function
 months.forEach((month,index)=>{
     console.log(`this is literal function ${month}`);
 });
 
+//Function constructor
 let functionconstructor = new Function("month","index","console.log(`This is function constructor and month is ${month}`)");
 months.forEach(functionconstructor);
 
