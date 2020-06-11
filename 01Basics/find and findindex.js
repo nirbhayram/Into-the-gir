25044174a06fbf86b9366401c4d82ad697d5b6e4
Nirbhay <da201501068@gmail.com>
2020-06-11 19:10:05 +0530

    find and findindex function in array.

---
+++ 01Basics/find and findindex.js
@@ -0,0 +1,30 @@
+console.log(Math.random());
+
+function Book(name, author){
+    this.name = name;
+    this.author = author;
+}
+
+let books = [new Book(`Javascript`,`Nirbhay`), new Book(`Java`,`Ram`)];
+
+let index = books.findIndex(function(book){
+    if(book.name==`Java`){
+        return true;
+    }
+    return false;
+});
+if(index>-1){
+    console.log(books[index]);
+}else{
+    console.log(`No match found`);
+}
+
+let book = books.find((book)=>{
+    if(book.author==`Nirbhay`){
+        return true;
+    }else{
+        return false;
+    }
+});
+
+console.log(book);
