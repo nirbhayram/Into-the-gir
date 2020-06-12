8df3f8d397f77297766caa68f97fa02cf1957015
Nirbhay <da201501068@gmail.com>
2020-06-12 23:20:27 +0530

    introduce to DOM

---
+++ 01Web/myscript.js
@@ -0,0 +1,6 @@
+// alert(`file is attached`);
+document.title = `Nirbhay it is changed.`
+document.getElementById(`idone`).textContent = `It is changed now from p>lorem10`;
+let paraIdone = document.querySelectorAll("#idone");
+console.log(paraIdone);
+//console.log(para);
