79c3e6fec8b8248d8f06457107abdffc305ef7d8
Nirbhay <da201501068@gmail.com>
2020-06-12 23:30:59 +0530

    Changing paragraph in HTML file using DOM concept.

--- 01Web/myscript.js
+++ 01Web/myscript.js
@@ -1,6 +1,10 @@
 // alert(`file is attached`);
 document.title = `Nirbhay it is changed.`
-document.getElementById(`idone`).textContent = `It is changed now from p>lorem10`;
-let paraIdone = document.querySelectorAll("#idone");
-console.log(paraIdone);
+// document.getElementById(`idone`).textContent = `It is changed now from p>lorem10`;
+// let paraIdone = document.querySelectorAll("#idone");
+// console.log(paraIdone);
 //console.log(para);
+
+
+let allPElement = document.querySelectorAll("p");
+allPElement.forEach((p)=>p.textContent="This is changed by for each loop in JS file");
