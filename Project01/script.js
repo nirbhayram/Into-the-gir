00d4277a99afa36df8a52d942f5bd13fbcf91821
Nirbhay <da201501068@gmail.com>
2020-06-13 17:54:55 +0530

    completed basic functionality of todo application for adding and removing task

--- Project01/script.js
+++ Project01/script.js
@@ -1,2 +1,58 @@
 // alert("Connected");
+function addToDo(){
+    let todo = document.querySelector("#input").value;
+    if(todo==""){
+        alert("please enter some text")
+        return;
+    }
+    let liElement = createListElement();
+    liElement = addInputIntoLiElement(liElement);
+    liElement = addLabelIntoLiElement(liElement,todo);
+    addListElementIntoList(liElement);
+    clearToDoText();
+}
 
+function clearToDoText(){
+    document.querySelector("#input").value = "";
+}
+
+function createListElement(){
+    let liElement = document.createElement("li");
+    liElement.className = "check";
+    return liElement;
+}
+
+function addInputIntoLiElement(liElement){
+    let inputCheckbox = document.createElement("input");
+    inputCheckbox.type="checkbox";
+    inputCheckbox.id="check";
+    liElement.appendChild(inputCheckbox);
+    return liElement;
+}
+
+function addLabelIntoLiElement(liElement,todo){
+    let labelText = document.createElement("label");
+    labelText.textContent=todo;
+    liElement.appendChild(labelText);
+    return liElement;
+}
+
+function addListElementIntoList(liElement){
+    let list = document.querySelector("#list");
+    list.appendChild(liElement);
+}
+
+function removeDoneToDo(){
+    let list = document.querySelector("#list");
+    let toRemoveList = [];
+    for (listElement of list.querySelectorAll("li")){
+        // console.log(listElement.childNodes[0].checked);
+        if(listElement.childNodes[0].checked){
+            toRemoveList.push(listElement);
+        }
+    }
+    // console.log(toRemoveList);
+    while(toRemoveList.length!=0){
+        list.removeChild(toRemoveList.pop());
+    }
+}
