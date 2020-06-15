6ba80409c5e40bec69588870e1ac2a852ae19b24
Nirbhay <da201501068@gmail.com>
2020-06-15 21:57:42 +0530

    Last commit to this folder.

--- two-website/src/App.js
+++ two-website/src/App.js
@@ -10,8 +10,8 @@ const App = () => {
         <div>
             <Navbar/>
             <HeroSection/>
-            <AppSection/>
             <CardSection/>
+            <AppSection/>
             <Footer/>
         </div>
     );
