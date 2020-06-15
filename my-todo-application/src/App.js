758b9b926c6f81354264757699629d615f1042d7
Nirbhay <da201501068@gmail.com>
2020-06-15 07:18:02 +0530

    Creating a "Clock" component with timer inside.

--- my-todo-application/src/App.js
+++ my-todo-application/src/App.js
@@ -1,26 +1,82 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+//
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+//
+// export default App;
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Clock extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: new Date()
+        }
+    }
+    componentDidMount() {
+        this.timerId = setInterval(()=>{this.updateTime()},1)
+    }
+    updateTime(){
+        this.setState({
+            time:new Date()
+        })
+    }
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+    render() {
+        return (
+            <div>
+                {this.state.time.toLocaleTimeString()}
+            </div>
+        );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        This is first <code>Application</code> created by <code>Nirbhay</code>.<br/>
+                        <Clock/>
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
