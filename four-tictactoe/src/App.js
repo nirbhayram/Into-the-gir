7260a701bc5ea08772ff0b35ee98dceaf63b66fc
Nirbhay <da201501068@gmail.com>
2020-06-17 14:02:37 +0530

    tic-tac-toe build-up

--- four-tictactoe/src/App.js
+++ four-tictactoe/src/App.js
@@ -1,37 +1,70 @@
-import React,{useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 import Icon from "./components/Icon"
 
-import {ToasContainer, toast} from "react-toastify"
+import { ToastContainer, toast } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css";
 
-import {Card, CardBody, Container, Button,Col,Row} from "reactstrap";
+import { Card, CardBody, Container, Button, Col, Row } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.css";
+
+import './App.css';
 
 const itemArray = new Array(9).fill("empty");
 
 function App() {
 
-	const [isCross,setIsCross] = useState("fasle");
-	const [winMessage,setWinMessage] = useState("");
+	const [isCross, setIsCross] = useState("fasle");
+	const [winMessage, setWinMessage] = useState("");
 
-	const reload = ()=>{
-	
+	const reload = () => {
+		setIsCross("false");
+		setWinMessage("");
+		itemArray.fill("empty");
 	}
 
 	const checkIsWinner = () => {
-	
+
 	}
 
-	const changeItem = (itemNumber)=>{
-	
+	const changeItem = (itemNumber) => {
+		if (winMessage) {
+			return toast(winMessage, { type:"success" })
+		} else {
+			if (itemArray[itemNumber] === "empty") {
+				isCross ? itemNumber[itemNumber] = "cross" : itemNumber[itemNumber] = "circle"
+				setIsCross(!isCross);
+			}
+			else {
+				return toast("Already filled", { type:"error" })
+			}
+		}
 	}
 
 	return (
-		<div className="App">
-			<Icon name="circle" />
-		</div>
+		<Container className="p-5">
+			<ToastContainer position="bottom-center" />
+			<Row>
+				<Col md={6} className="offset-md-3">
+					{winMessage ? (
+						<div>
+							<h2 className="text-primary text-uppercase text-center">{winMessage}</h2>
+							<Button className="md-2" color="success" block onClick={reload}>Reload the game</Button>
+						</div>
+					) : (
+							<h2 className="text-center text-warning">{isCross?("Cross"):("Circle")}'s turn</h2>
+						)}
+					<div className="grid">
+						{itemArray.map((item, index) => (
+							<Card>
+								<CardBody className="box">
+									<Icon name={item} />
+								</CardBody>
+							</Card>
+						))}
+					</div>
+				</Col>
+			</Row>
+		</Container>
 	);
 }
 
