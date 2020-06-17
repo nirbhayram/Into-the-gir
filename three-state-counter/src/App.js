15b05d2285afcdd006544557af441d357f1f4d0d
Nirbhay <da201501068@gmail.com>
2020-06-17 08:26:08 +0530

    Created App componenet.

--- three-state-counter/src/App.js
+++ three-state-counter/src/App.js
@@ -1,17 +1,17 @@
-// import React,{useState} from 'react';
-// import logo from './logo.svg';
-// import './App.css';
+import React,{useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-// function App() {
-//   const [count , setCount] = useState(0);
-//   return (
-//     <div className="App">
-//       <h2>The counter is {count}</h2>
-//       <button onClick={()=>setCount(0)}>Reset</button>
-//       <button onClick={()=> count>9?"":setCount(count+1)} >Increment</button>
-//       <button onClick={()=>count<1?"":setCount(count-1)}>Decrement</button>
-//     </div>
-//   );
-// }
+function App() {
+  const [count , setCount] = useState(0);
+  return (
+    <div className="App">
+      <h2>The counter is {count}</h2>
+      <button onClick={()=>setCount(0)}>Reset</button>
+      <button onClick={()=> count>9?"":setCount(count+1)} >Increment</button>
+      <button onClick={()=>count<1?"":setCount(count-1)}>Decrement</button>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
