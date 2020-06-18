507686ca57ccbe95b8a925864bfc96ee2e6b8f62
Nirbhay <da201501068@gmail.com>
2020-06-18 22:17:13 +0530

    Created context provider.

---
+++ side-one-context/src/Provider.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react"
+
+import PackageContext from "./Context";
+
+const Provider = (props)=>{
+    const [mission, setMission] = useState({
+        mname: "Got ot russia",
+        agent: 007,
+        isAccepted: "not accepted"
+    })
+    return (
+        <PackageContext.Provider value={{
+            data: mission,
+            acceptTheMission: ()=>{
+                setMission({...mission, isAccepted:"accepted"})
+            }
+        }}>
+            {props.children}
+        </PackageContext.Provider>
+    )
+}
