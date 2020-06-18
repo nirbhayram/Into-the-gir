33e9f475849ba8c4074176c0291409df0491f520
Nirbhay <da201501068@gmail.com>
2020-06-18 21:56:07 +0530

    Props drilling

---
+++ side-one-context/src/Problem.js
@@ -0,0 +1,27 @@
+// import React, { useState } from "react"
+
+// const GrandChild = (props) => {
+//   return (
+//     <h3>GrandChild: {props.brand}</h3>
+//   );
+// }
+
+// const Child = (props) => {
+//   return (
+//     <div>
+//       <h2>Child </h2>
+//       <GrandChild brand={props.brand}/>
+//     </div>
+//   );
+// }
+
+// const App = () => {
+//   const [brandname] = useState("Amazon")
+//   return (
+//     <div>
+//       <h1>Hello</h1>
+//       <Child brand={brandname}></Child>
+//     </div>
+//   );
+// }
+// export default App
