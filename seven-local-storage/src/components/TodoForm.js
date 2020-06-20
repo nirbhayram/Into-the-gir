87f93683e6ba5453408319258a1ee1557aea8bf8
Nirbhay <da201501068@gmail.com>
2020-06-20 11:21:28 +0530

    todo form created to enter todo task

--- seven-local-storage/src/components/TodoForm.js
+++ seven-local-storage/src/components/TodoForm.js
@@ -1,1 +1,44 @@
 import React, {useState} from "react"
+import {Button, Form, FormGroup, Input, InputGroup, InputGroupAddon} from "reactstrap"
+
+const TodoForm = (addTodos) => {
+    const [todoString, setTodoString] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (todoString === "") {
+            return alert("Please enter some todo task.");
+        }
+        const todo = {
+            todoString,
+            id: v4()
+        }
+        addTodos(todo);
+        setTodoString("")
+    }
+    return (
+        <Form>
+            <FormGroup>
+                <InputGroup>
+                    <Input
+                        type="text"
+                        placeholder="Enter your todo"
+                        name="todo"
+                        id="todo"
+                        value={todoString}
+                        onChange={(e) => setTodoString(e.target.value)}
+                    >
+                    </Input>
+                    <InputGroupAddon addonType={"prepend"}>
+                        <Button onClick={handleSubmit}>
+                            Add todo
+                        </Button>
+                    </InputGroupAddon>
+                </InputGroup>
+            </FormGroup>
+        </Form>
+    );
+
+}
+
+export default TodoForm
