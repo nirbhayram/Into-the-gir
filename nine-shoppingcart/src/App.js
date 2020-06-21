5230d359b22f94bd812b493d85aa7b288566e580
Nirbhay <da201501068@gmail.com>
2020-06-21 14:52:45 +0530

    Toast working fixed

--- nine-shoppingcart/src/App.js
+++ nine-shoppingcart/src/App.js
@@ -1,38 +1,47 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { toast } from 'react-toastify';
 import BuyPage from './Components/BuyPage';
 import "bootstrap/dist/css/bootstrap.min.css"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
+  toast.configure();
   const [items, setItems] = useState([]);
 
-  const addItemToCart = (item)=>{
-    const isAlreadyAdded = items.find((i)=>i.id===item.id)
-    if (isAlreadyAdded!==-1) {
-      return toast("Item is already in cart",{
-        type:"error"
+  const addItemToCart = (item) => {
+    const isAlreadyAdded = items.findIndex((i) => i.id === item.id)
+    if (isAlreadyAdded !== -1) {
+      toast.error('Item is already in the cart', {
+        position: "bottom-center",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
       });
+      return
     }
-    setItems([...items,item])
+    setItems([...items, item]);
   }
 
-  const removeItem = (id)=>{
-    setItems(items.filter((item)=>item.id!==id))
+  const removeItem = (id) => {
+    setItems(items.filter((item) => item.id !== id))
   }
 
-  const purchaseItems = ()=>{
+  const purchaseItems = () => {
     toast("All items purchased.", {
-      type:"success"
+      type: "success"
     });
     setItems([]);
   }
 
   return (
     <div className="App">
-      <BuyPage addItemToCart={addItemToCart}/>
+      <BuyPage addItemToCart={addItemToCart} />
     </div>
   );
 }
