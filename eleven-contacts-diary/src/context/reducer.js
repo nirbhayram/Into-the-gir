afa8bb36f4be84d6d7108dfdbf6861e66edfe67f
Nirbhay <da201501068@gmail.com>
2020-06-22 23:14:50 +0530

    Adding use reducers

--- eleven-contacts-diary/src/context/reducer.js
+++ eleven-contacts-diary/src/context/reducer.js
@@ -1,4 +1,3 @@
-//TODO: create contact using all actions
 
 import {
   SET_CONTACT,
@@ -7,4 +6,19 @@ import {
   SET_SINGLE_CONTACT
 } from "./action.types";
 
-//TODO: use switch case
+
+export default (state,action)=>{
+  switch (action.type) {
+    case SET_CONTACT:
+      return action.payload === null ? {...state, contact:[]} : {...state, contact: action.payload} 
+      break;
+    case  SET_LOADING:
+      return {...state, isLoading: action.payload}
+    case CONTACT_TO_UPDATE:
+      return {...state, contactToUpdate: action.payload, contactToUpdateKey: action.key}
+    case SET_SINGLE_CONTACT:
+      return {...state, contact: action.payload}
+    default:
+      break state;
+  }
+}
