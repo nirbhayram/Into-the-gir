7fb9fb6d365a4fb09c1a5f4d9c25daf089c9c3ca
Nirbhay <da201501068@gmail.com>
2020-06-25 00:07:36 +0530

    Changing the project to base requirement.

--- ecommerce-gatsby-project/src/components/layout.js
+++ ecommerce-gatsby-project/src/components/layout.js
@@ -9,10 +9,11 @@ import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import "./bootstrap.min.css"
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
+	const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
         siteMetadata {
@@ -22,29 +23,15 @@ const Layout = ({ children }) => {
     }
   `)
 
-  return (
-    <>
-
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
-  )
+	return (
+		<div>
+			<main>{children}</main>
+		</div>
+	)
 }
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 }
 
 export default Layout
