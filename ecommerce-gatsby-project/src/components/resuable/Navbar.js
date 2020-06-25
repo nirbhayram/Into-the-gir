355817411aa38123af57495b62d19943aa414637
Nirbhay <da201501068@gmail.com>
2020-06-25 23:01:44 +0530

    Added toggler on click functionality

--- ecommerce-gatsby-project/src/components/resuable/Navbar.js
+++ ecommerce-gatsby-project/src/components/resuable/Navbar.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react'
-import {Link} from 'gatsby'
+import { Link } from 'gatsby'
 import logo from '../../images/logo.png'
-import {FaCartArrowDown} from 'react-icons/fa'
+import { FaCartArrowDown } from 'react-icons/fa'
 
 
 export default class Navbar extends Component {
+    state = {
+        navbarState: false,
+        navbarClass: "collapse navbar-collapse"
+    }
+    myToggler = () => {
+        this.state.navbarState ?
+            this.setState({
+                navbarState: false,
+                navbarClass: "collapse navbar-collapse"
+            }) : this.setState({
+                navbarState: true,
+                navbarClass: "collapse navbar-collapse show"
+            })
+    }
     render() {
         return (
             <nav className="navbar navbar-expand-sm bg-theme text-white">
                 <Link to="/" className="navbar-brand ml-5">
-                    <img src={logo} alt="logo goes here" width="40px"/>
+                    <img src={logo} alt="logo goes here" width="40px" />
                 </Link>
-                <button className="navbar-toggler" type="button">
-                    <span className="text-white">Menu</span>
+                <button className="navbar-toggler" type="button" onClick={this.myToggler}>
+                    <span className="text-white">menu</span>
                 </button>
-                <div className="collapse navbar-collapse show">
-                    <ul className="navbar-nav ml-auto mr-5">
+                <div className={this.state.navbarClass}>
+                    <ul className="navbar-nav ml-auto mr-3">
                         <li className="nav-item">
                             <Link to="/" className="nav-link text-white">Home</Link>
                         </li>
@@ -24,7 +38,7 @@ export default class Navbar extends Component {
                         </li>
                         <li className="nav-item">
                             <Link to="/" className="nav-link text-white">
-                                <FaCartArrowDown className="cart-class"/>
+                                <FaCartArrowDown className="cart-class" />
                             </Link>
                         </li>
                     </ul>
