753723a6d4c278d07a050bdf113b484a846eb66e
Nirbhay <da201501068@gmail.com>
2020-06-27 13:15:54 +0530

    Contentful space id and access token added

--- ecommerce-gatsby-project/gatsby-config.js
+++ ecommerce-gatsby-project/gatsby-config.js
@@ -27,6 +27,15 @@ module.exports = {
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
+
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
